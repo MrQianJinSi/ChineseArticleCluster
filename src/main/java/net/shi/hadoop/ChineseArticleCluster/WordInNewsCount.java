748e53090386b133392febf45b32b7b5a4fc87bb
Galo <galoiscode@gmail.com>
2015-09-09 16:06:44 +0800

    修改Map函数的输出key为TextPairWritable.

--- src/main/java/net/shi/hadoop/ChineseArticleCluster/WordInNewsCount.java
+++ src/main/java/net/shi/hadoop/ChineseArticleCluster/WordInNewsCount.java
@@ -15,15 +15,16 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.SequenceFileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 public class WordInNewsCount extends Configured implements Tool {
-	private static final String INPUT_PATH = "/home/galois/workspace/SougouNewsInput/C000008_bak";
+	private static final String INPUT_PATH = "/home/galois/workspace/SougouNewsInput/C000008_bak/*.txt";
 	private static final String OUTPUT_PATH = "/home/galois/workspace/SougouNewsOutput/wordCount";
 	
 	public static class WordInNewsCountMapper
-	extends Mapper<Text, Text, Text, IntWritable>{
+	extends Mapper<Text, Text, TextPairWritable, IntWritable>{
 		private IntWritable one = new IntWritable(1);
 		
 		public void map(Text key, Text value,
@@ -32,16 +33,15 @@ public class WordInNewsCount extends Configured implements Tool {
 	    	ListIterator<Term> iter = termList.listIterator();
 	    	
 	    	while(iter.hasNext()){
-	    		String outputKey = iter.next() + "@" + key;
-	    		context.write(new Text(outputKey), one);
+	    		context.write(new TextPairWritable(iter.next().toString(), key), one);
 	    	}
 		}
 	}
 
 	public static class WordInNewsCountReducer
-	extends Reducer<Text, IntWritable, Text, IntWritable>{
+	extends Reducer<TextPairWritable, IntWritable, TextPairWritable, IntWritable>{
 		
-		public void reduce(Text key, Iterable<IntWritable> values,
+		public void reduce(TextPairWritable key, Iterable<IntWritable> values,
 				Context context) throws IOException, InterruptedException{
 			int count = 0;
 			
@@ -58,22 +58,26 @@ public class WordInNewsCount extends Configured implements Tool {
 		job.setJarByClass(WordInNewsCount.class);
 		
 		job.setInputFormatClass(NewsInputFormat.class);
+//		job.setOutputFormatClass(SequenceFileOutputFormat.class);
 		
 		job.setMapperClass(WordInNewsCountMapper.class);
 		job.setReducerClass(WordInNewsCountReducer.class);
 		
-		job.setOutputKeyClass(Text.class);
+		job.setOutputKeyClass(TextPairWritable.class);
 		job.setOutputValueClass(IntWritable.class);
 		
-		FileInputFormat.addInputPath(job, new Path(INPUT_PATH));
-		FileOutputFormat.setOutputPath(job, new Path(OUTPUT_PATH));
+//		job.setNumReduceTasks(2);
+		
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
 		
 		return job.waitForCompletion(true) ? 0 : 1;
 	}
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		int exitCode = ToolRunner.run(new WordInNewsCount(), args);
+		String[] paths = {INPUT_PATH, OUTPUT_PATH};
+		int exitCode = ToolRunner.run(new WordInNewsCount(), paths);
 		
 		System.exit(exitCode);
 	}
